4139e4e5c2cce92ba90e5c226229f43e79337740
LetsGitFunky <NickHeinBass@gmail.com>
2023-05-29 18:39:27 -0700

    practice mode implemented

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import * as Tone from 'tone';
 let game;  // Variable to hold the current game
 
 document.addEventListener("DOMContentLoaded", () => {
+    game = new Game();
+    attachEventListeners(game);  // Attach the event listeners to the page elements
+
     const newGameButton = document.getElementById('new-game');
 
     newGameButton.addEventListener('click', async () => {
-        game = new Game();
-        console.log(game)
-        attachEventListeners(game);  // Attach the event listeners to the page elements
+        game.startNewGame();
+    });
+
+    const practiceModeButton = document.getElementById('practice-mode');
+
+    practiceModeButton.addEventListener('click', () => {
+        game.togglePracticeMode();
     });
 });
--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,11 +1,11 @@
 import { playMelody, playNote, playTriumphSound } from './sound.js';
 
 const melodies = [
-    ["C4", "D4"]
-    ["C4", "E4", "D4", "F4", "E4", "D4", "C4"],
-    ["C4", "G4", "A4", "G4", "E4", "F4", "D4", "E4", "C4"],
-    ["C4", "C5", "B4", "G4", "A4", "B4", "C5"],
-    
+    ["C4", "D4"],
+    // ["C4", "E4", "D4", "F4", "E4", "D4", "C4"],
+    // ["C4", "G4", "A4", "G4", "E4", "F4", "D4", "E4", "C4"],
+    // ["C4", "C5", "B4", "G4", "A4", "B4", "C5"],
+
 ];
 
 export class Game {
@@ -15,16 +15,22 @@ export class Game {
         this.userMelody = [];
         this.userMelodyTemp = [];
         this.isPlaying = false;
+        this.isPracticeMode = true;
         // this.earTrainingMode = false;
-        this.startNewGame();
+        // this.startNewGame();
+    }
+
+    togglePracticeMode() {
+        this.isPracticeMode = !this.isPracticeMode;
     }
 
     startNewGame() {
-        let randomIndex = Math.floor(Math.random() * melodies.length); // comment out for youWin testing
+        let randomIndex = Math.floor(Math.random() * melodies.length); // comment out for youWin testingasdg
         this.currentMelody = melodies[randomIndex]; // comment out for youWin testing
         this.hideMessage();
         this.userMelody = [];
         this.userMelodyTemp = [];
+        this.isPracticeMode = false;
         setTimeout(() => this.playCurrentMelody(), 500);
     }
 
@@ -53,28 +59,33 @@ export class Game {
     handleNotePlayed(note) {
         if (this.isPlaying) return;
 
-        this.userMelodyTemp.push(note);
-        let userMelodyString = this.userMelodyTemp.join("");
-        let currentMelodyString = this.currentMelody.slice(0, this.userMelodyTemp.length).join("");
-
-        if (userMelodyString === currentMelodyString) {
-
-            if (this.userMelodyTemp.length === this.userMelody.length + 1) {
-                // The user successfully entered the sequence, move to the next round
-                this.userMelody = [...this.userMelodyTemp];
-                this.userMelodyTemp = []; // Reset temporary melody
-
-                // Check if the user has won the game
-                if (this.userMelody.length === this.currentMelody.length) {
-                    this.youWin();
-                } else {
-                    setTimeout(() => this.playCurrentMelody(), 500);
-                }
-            }
+        if (this.isPracticeMode) {
+            playNote(note);
         } else {
-            // The user made a mistake, reset the round
-            this.userMelodyTemp = [];
-            setTimeout(() => this.playCurrentMelody(), 500);
+
+            this.userMelodyTemp.push(note);
+            let userMelodyString = this.userMelodyTemp.join("");
+            let currentMelodyString = this.currentMelody.slice(0, this.userMelodyTemp.length).join("");
+
+            if (userMelodyString === currentMelodyString) {
+
+                if (this.userMelodyTemp.length === this.userMelody.length + 1) {
+                    // The user successfully entered the sequence, move to the next round
+                    this.userMelody = [...this.userMelodyTemp];
+                    this.userMelodyTemp = []; // Reset temporary melody
+
+                    // Check if the user has won the game
+                    if (this.userMelody.length === this.currentMelody.length) {
+                        this.youWin();
+                    } else {
+                        setTimeout(() => this.playCurrentMelody(), 500);
+                    }
+                }
+            } else {
+                // The user made a mistake, reset the round
+                this.userMelodyTemp = [];
+                setTimeout(() => this.playCurrentMelody(), 500);
+            }
         }
     }
 
